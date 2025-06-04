1ff463d5cab4b7f7e3dae49bd0badab2cb7b830c
aryanmaftooh <ariyanmaftooh@gmail.com>
2025-06-04 19:10:59 +0330

    Able tp handle any number of requests

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import socket
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+PORT = 65433  # Port to listen on (non-privileged ports are > 1023)
 body = (
     "<html>\n"
-    "  <head><title>Example Page</title></head>\n"
+    "  <head><title>debugging</title></head>\n"
     "  <body>\n"
-    "    <h1>Hello, world!</h1>\n"
-    "    <p>heiui ddduh bnudr  9prh d4p rfhir fhih efu hurhepfe hf</p>\n"
+    "    <h1>TRY ME</h1>\n"
+    "    <p>I am in the debugging phase</p>\n"
     "  </body>\n"
     "</html>\n"
 )
@@ -22,12 +22,13 @@ response = (
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall((response + body).encode("utf-8"))
+    s.listen(5)
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall((response + body).encode("utf-8"))
