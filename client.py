8b685872bda774bee484c51f4f58a46f89859874
aryanmaftooh <ariyanmaftooh@gmail.com>
2025-06-04 18:41:51 +0330

    I fixed a bug and the body of http response can have any fixed length

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
-HOST = "chatgpt.com"  # The server's hostname or IP address
-PORT = 80  # The port used by the server
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
 httpRequest = b'GET / HTTP/1.1\r\nHost: chatgpt.com:8080\r\nConnection: keep-alive\r\nsec-ch-ua: "Chromium";v="136", "Microsoft Edge";v="136", "Not.A/Brand";v="99"\r\nsec-ch-ua-mobile: ?0\r\nsec-ch-ua-platform: "Windows"\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br, zstd\r\nAccept-Language: en-US,en;q=0.9,en-CA;q=0.8\r\n\r\n'
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
--- server.py
+++ server.py
@@ -2,19 +2,21 @@ import socket
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-response = (
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/html; charset=UTF-8\r\n"
-    "Content-Length: 138\r\n"
-    "Connection: close\r\n"
-    "\r\n"
+body = (
     "<html>\n"
     "  <head><title>Example Page</title></head>\n"
     "  <body>\n"
     "    <h1>Hello, world!</h1>\n"
-    "    <p>This is an example of an HTTP response body.</p>\n"
+    "    <p>heiui ddduh bnudr  9prh d4p rfhir fhih efu hurhepfe hf</p>\n"
     "  </body>\n"
     "</html>\n"
+)
+response = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/html; charset=UTF-8\r\n"
+    f"Content-Length: {len(body)}\r\n"
+    "Connection: close\r\n"
+    "\r\n"
 )
 
 
@@ -28,5 +30,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
-            print(data.decode("utf-8"))
-            conn.sendall(response.encode("utf-8"))
+            conn.sendall((response + body).encode("utf-8"))
