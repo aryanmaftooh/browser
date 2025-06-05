a89d6ac4fa4012467b54e1b7b3062e816046c08c
aryanmaftooh <ariyanmaftooh@gmail.com>
2025-06-05 19:05:05 +0330

    Added features to client.py

--- client.py
+++ client.py
@@ -1,11 +1,46 @@
 import socket
+import sys
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
-httpRequest = b'GET / HTTP/1.1\r\nHost: chatgpt.com:8080\r\nConnection: keep-alive\r\nsec-ch-ua: "Chromium";v="136", "Microsoft Edge";v="136", "Not.A/Brand";v="99"\r\nsec-ch-ua-mobile: ?0\r\nsec-ch-ua-platform: "Windows"\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br, zstd\r\nAccept-Language: en-US,en;q=0.9,en-CA;q=0.8\r\n\r\n'
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(httpRequest)
-    data = s.recv(1024)
 
-print(f"{data!r}")
+def sendRequest():
+    HOST = "142.250.72.206"  # The server's hostname or IP address
+    PORT = 80  # The port used by the server
+    httpRequest = (
+        "GET / HTTP/1.1\r\n"
+        "Host: facebook\r\n"
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)\r\n"
+        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+        "Accept-Language: en-US,en;q=0.5\r\n"
+        "Accept-Encoding: gzip, deflate\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+    )
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(httpRequest.encode("utf-8"))
+        m = s.recv(1024)
+        while True:
+            data = s.recv(1024)
+            if data == b"":
+                break
+            else:
+                m = m + data
+        return m.decode("utf-8")
+
+    # return data.decode("utf-8")
+
+
+if __name__ == "__main__":
+    response_output = open("httpRecieved.html", "w")
+    sys.stdout = response_output
+    r = sendRequest()
+    nlc = 0
+    for i in range(len(r)):
+        if r[i] == "\n":
+            nlc += 1
+        if nlc == 6:
+            p = i
+            break
+    print(r[p::])
+    response_output.close()
